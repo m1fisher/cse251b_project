4cab1cf5676cb2b2ba148fc1ee1224215eeb7f46
Marshall Fisher <m1fisher@ucsd.edu>
2025-05-21 17:42:51 -0700

    add helpers.py

---
+++ source/helpers.py
@@ -0,0 +1,23 @@
+
+def path_length_ratio(data):
+    """
+    GPT4o generated. Makes some assumptions about input dimensionality.
+
+    data: np.ndarray
+    """
+    deltas   = np.diff(data, axis=2)                   # shape (10000,50,109,2)
+    step_d   = np.linalg.norm(deltas, axis=-1)        # (10000,50,109)
+    L        = step_d.sum(axis=-1)                    # (10000,50)
+    start    = train_data[..., 0, :]                         # (10000,50,2)
+    end      = train_data[..., -1, :]                        # (10000,50,2)
+    D        = np.linalg.norm(end - start, axis=-1)   # (10000,50)
+
+    # avoid division by zero
+    with np.errstate(divide='ignore', invalid='ignore'):
+        nonlin = L / D
+
+    # set zero-distance sequences to zero
+    nonlin = np.where(D==0, 0.0, nonlin)
+
+    return nonlin
+

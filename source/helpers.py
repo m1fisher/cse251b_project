065620cbac2473796d60e611b0a386ae5fc83e39
Marshall Fisher <m1fisher@ucsd.edu>
2025-05-21 17:45:07 -0700

    fix typo train_data -> data

--- source/helpers.py
+++ source/helpers.py
@@ -8,8 +8,8 @@ def path_length_ratio(data):
     deltas   = np.diff(data, axis=2)                   # shape (10000,50,109,2)
     step_d   = np.linalg.norm(deltas, axis=-1)        # (10000,50,109)
     L        = step_d.sum(axis=-1)                    # (10000,50)
-    start    = train_data[..., 0, :]                         # (10000,50,2)
-    end      = train_data[..., -1, :]                        # (10000,50,2)
+    start    = data[..., 0, :]                         # (10000,50,2)
+    end      = data[..., -1, :]                        # (10000,50,2)
     D        = np.linalg.norm(end - start, axis=-1)   # (10000,50)
 
     # avoid division by zero
